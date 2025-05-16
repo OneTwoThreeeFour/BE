47d7e14deb1b70540cf237f98643ee6f3feab1c3
erika0915 <soohee2001@gmail.com>
2025-05-16 14:04:04 +0900

    Feat : 프로젝트 초기 세팅

---
+++ src/main/java/onetwothreefour/Ondam/OndamApplication.java
@@ -0,0 +1,13 @@
+package onetwothreefour.Ondam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OndamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OndamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/onetwothreefour/Ondam/OndamApplicationTests.java
@@ -0,0 +1,13 @@
+package onetwothreefour.Ondam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OndamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
